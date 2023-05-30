3f354b6f62c369a118b3bb9809dbf88980b00406
Alexandra Vachon <alvachon@student.42quebec.com>
2023-05-30 15:25:58 -0400

    namespace funsies

--- 00/ex01/PhoneBook.cpp
+++ 00/ex01/PhoneBook.cpp
@@ -6,11 +6,37 @@
 /*   By: alvachon <alvachon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 11:22:35 by alvachon          #+#    #+#             */
-/*   Updated: 2023/05/30 12:39:56 by alvachon         ###   ########.fr       */
+/*   Updated: 2023/05/30 15:25:23 by alvachon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+
+//https://www.youtube.com/watch?v=ts1Eek5w7ZA
+
+namespace   addProg {
+
+    void    print(const char *test)
+    {
+        std::cout << "Printing from add" << test;
+    }
+}
+
+namespace   searchProg {
+
+    void    print(const char *test)
+    {
+        std::cout << "Printing from search" << test;
+    }
+}
+
+namespace   exitProg {
+
+    void    print(const char *test)
+    {
+        std::cout << "Printing from exit" << test;
+    }
+}
 
 const char    *StartProgram(int argc, const char *argv[])
 {
@@ -33,7 +59,14 @@ int main(int argc, const char *argv[])
     const char    *cmd = StartProgram(argc, argv);
 
     if (cmd != NULL)
-        std::cout << "Starting " << cmd;
+    {
+        std::cout << "Starting " << cmd << '\n';
+        exitProg::print("TEST");
+        std::cout << '\n';
+        searchProg::print("TEST");
+        std::cout << '\n';
+        addProg::print("TEST");
+    }
     std::cout << '\n';
     return (0);
 }
