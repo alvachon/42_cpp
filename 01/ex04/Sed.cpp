039bfe52c6e98b5b50f22b1edb5380ad36bd261e
Alexandra Vachon <alvachon@student.42quebec.com>
2023-07-20 11:18:14 -0400

    final ex04

---
+++ 01/ex04/Sed.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sed.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvachon <alvachon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/31 13:50:00 by alvachon          #+#    #+#             */
+/*   Updated: 2023/07/20 11:18:00 by alvachon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Sed.h"
+
+Sed::Sed(std::string av1, std::string av2, std::string av3) :
+    newFile_(av1), wordIn_(av2), wordOut_(av3)   {
+
+    return ;
+}
+
+Sed::~Sed(void)   {
+
+    return ;
+}
+
+void    Sed::doTheThing(void)   {
+
+        input_.open(newFile_);
+        newFile_.append(".replace");
+        if (input_)
+        {
+            output_.open(newFile_);
+            while (!input_.eof())
+            {
+                getline(input_, str_);
+                for (unsigned i = 0; i < str_.length(); ++i)
+                {
+                    if (str_[i] == wordIn_[0])
+                    {
+                        while (str_.compare(i, wordIn_.length(), wordIn_) == 0)
+                        {
+                            output_ << wordOut_;
+                            i += wordIn_.length();
+                        }
+                    }
+                    if (str_[i])
+                        output_ << str_[i];
+                }
+                output_ << '\n';
+            }
+            input_.close();
+            output_.close();
+        }
+}
--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -6,46 +6,18 @@
 /*   By: alvachon <alvachon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 14:07:53 by alvachon          #+#    #+#             */
-/*   Updated: 2023/07/19 15:52:50 by alvachon         ###   ########.fr       */
+/*   Updated: 2023/07/20 11:14:15 by alvachon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cstddef> // std::size_t
+#include "Sed.h"
 
 int main(int ac, char **av)
 {
     if (ac == 4)
     {
-        std::ifstream   ifs(av[1]);
-        std::string     newFile = av[1];
-        std::ofstream   ofs(newFile.append(".replace"));
-        std::string     line;
-        std::size_t     start;
-        std::size_t     len;
-
-    //Chercher halp demain matin.
-    // ifs.open();
-    // ofs.open();
-        while (ifs.good()) {
-            getline(ifs, line);
-            start = line.find(av[2]);
-            if (start != std::string::npos)
-            {
-                line.begin()+start;
-                len = line.find_first_not_of(av[2]);
-                line.erase(len);
-                line.insert(start, av[3]);
-                line.rbegin();
-            }
-            ofs << line;
-        }
-        ofs << '\n';
-        ifs.close();
-        ofs.close();
-
+        Sed sed(av[1], av[2], av[3]);
+        sed.doTheThing();
     }
     return (0);
 }   
