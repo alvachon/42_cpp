b60b67bef4a2b282cd8c8ca71c17f0e596cbb1d6
alvachon <alvachon@student.42quebec.com>
2023-11-23 14:19:36 +0000

    ex01

--- 07_Template/ex01/iter.h
+++ 07_Template/ex01/iter.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   iter.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alvachon <alvachon@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 12:41:20 by alvachon          #+#    #+#             */
-/*   Updated: 2023/11/16 14:44:41 by alvachon         ###   ########.fr       */
+/*   Updated: 2023/11/23 14:15:51 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,22 @@
 # define ITER_H
 
 #include <iostream>
-#include <cstdio>
+#include <string>
 
-template<typename element>
-void iter(element* x, size_t length, void (function)(element&))
+template<typename Function>
+void ident(Function target)
 {
-    std::cout << "Adress:" << x << std::endl;
-    std::cout << "Size" << length << std::endl;  
+	std::cout << target << std::endl;
+}
+
+template<typename T, typename Function>
+void iter(T *array, int length, Function f)
+{
+    for (int i = 0; i < length; i++)
+    {
+        std::cout << "array[" << i << "] = " << array[i] << std::endl;
+        f(array[i]);
+    }
     return ; 
 }
 
--- 07_Template/ex01/main.cpp
+++ 07_Template/ex01/main.cpp
@@ -3,24 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alvachon <alvachon@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 12:40:44 by alvachon          #+#    #+#             */
-/*   Updated: 2023/11/16 14:43:59 by alvachon         ###   ########.fr       */
+/*   Updated: 2023/11/23 14:18:04 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "iter.h"
 
-void	ident(int element)
-{
-	std::cout << element << std::endl;
-}
 
 int main( void )
 {
-	int tableau[4] = {1, 2, 3};
+	int tableau[]			= {1, 2, 3};
+	std::string arrStr[] 	= {"Hello", "World", "!"};
 
-	iter(tableau, sizeof(tableau), ident(tableau[0]));
+	iter(tableau, sizeof(tableau)/sizeof(int), ident<int>);
+	iter(arrStr, sizeof(arrStr)/sizeof(std::string), ident<std::string>);
 	return 0;
 }
