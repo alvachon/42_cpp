1ed50e1fa942bfba35421875bc034fdaaddf3e2c
alvachon <alvachon@student.42quebec.com>
2023-11-30 19:39:42 +0000

    07

--- 07_Template/ex02/Array.h
+++ 07_Template/ex02/Array.h
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 12:41:20 by alvachon          #+#    #+#             */
-/*   Updated: 2023/11/23 14:50:56 by codespace        ###   ########.fr       */
+/*   Updated: 2023/11/30 19:34:41 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,40 @@
 #include <iostream>
 #include <string>
 
-template<typename Function>
+template<typename A>
 class Array {
-
     public:
-        Array();
-        virtual ~Array();
-        Array(Array & src, const std::string target);
-        Array& operator=(const Array & rhs);
+    // Construction sans paramètres : crée un array vide.
+        Array(void) : _size(0), _array(new A[0]) { std::cout << "Constructor No Params" << std::endl; return; }
+    // Construction avec un paramètre de type unsigned int n
+        Array(unsigned int n) : _size(n), _array(new A[n]) { std::cout << "Constructor w/ Default Init" << std::endl; return; }
+    // Construction par recopie
+        Array(const Array & src) : _array(NULL) { *this = src; std::cout << "Copy constructor" << std::endl; return; }
+    // Surcharge de l’opérateur d’affectation.
+        Array& operator=(const Array & rhs) {
+            if (this != &rhs) {
+                this->_size = rhs._size;
+                delete [] this->_array;
+                this->_array = new A[rhs._size];
+    
+                for (unsigned int i = 0; i < rhs._size; i++){
+                    this->_array[i] = rhs._array[i];
+                }
+            std::cout << "Surcharge called" << std::endl;   
+            }
+            return *this;
+        }
+        virtual ~Array(void) { delete [] this->_array; std::cout << "Deletion ..." << std::endl; return; };
+
+        A& operator[](unsigned int i) const {
+            if (i >= this->_size)
+                throw std::exception();
+            return this->_array[i];
+        }
 
     private:
-        Array();
+        unsigned int    _size;
+        A               *_array;
 };
 
 #endif/* ************************************************** ARRAY_H */
--- 07_Template/ex02/main.cpp
+++ 07_Template/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 12:40:44 by alvachon          #+#    #+#             */
-/*   Updated: 2023/11/23 14:51:01 by codespace        ###   ########.fr       */
+/*   Updated: 2023/11/30 19:38:57 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,51 @@
 
 int main( void )
 {
-	Array<std::string>	arrStr(3);
+	{	// Test 1 : Construction sans paramètres : crée un array vide.
+		Array<char>	tab;
+	}
+	{	// Test 2 : Construction avec un paramètre de type unsigned int n
+		Array<char>	tab(5);
 
+		tab[0] = 'H';
+		tab[1] = 'e';
+		tab[2] = 'l';
+		tab[3] = 'l';
+		tab[4] = 'o';
+
+		for (int i = 0; i < 5; i++)
+			std::cout << "tab: " << tab[i] << std::endl;
+	}
+	{
+		Array<char>	tab(5);
+		Array<char>	tabCopy(tab);
+
+		tab[0] = 'H';
+		tab[1] = 'e';
+		tab[2] = 'l';
+		tab[3] = 'l';
+		tab[4] = 'o';
+
+		Array<char>	tabCopy2(tab);
+
+		for (int i = 0; i < 5; i++)
+			std::cout << "tab: " << tab[i] << std::endl;
+		for (int i = 0; i < 5; i++)
+			std::cout << "tabCopy: " << tabCopy[i] << std::endl;
+		for (int i = 0; i < 5; i++)
+			std::cout << "tabCopy2: " << tabCopy2[i] << std::endl;
+		
+		tabCopy[0] = 'W';
+		tabCopy[1] = 'o';
+		tabCopy[2] = 'r';
+		tabCopy[3] = 'l';
+		tabCopy[4] = 'd';
+
+		for (int i = 0; i < 5; i++)
+			std::cout << "tab: " << tab[i] << std::endl;
+		for (int i = 0; i < 5; i++)
+			std::cout << "tabCopy: " << tabCopy[i] << std::endl;
+
+	}
 	return 0;
 }
